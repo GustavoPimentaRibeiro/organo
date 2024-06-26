2fb6845fc54c6e5f34973130b3007e83e8ff0114
Gustavo Pimenta Ribeiro <63890172+GustavoPimentaRibeiro@users.noreply.github.com>
2024-06-26 17:25:23 -0300

    aplicacao completa

---
+++ src/components/Banner/index.js
@@ -0,0 +1,14 @@
+import "./Banner.css";
+
+const Banner = () => {
+  return (
+    <header className="banner">
+      <img
+        src="/images/banner.png"
+        alt="Banner principal da página da Organo"
+      />
+    </header>
+  );
+};
+
+export default Banner;
---
+++ src/components/CampoTexto/index.js
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import "./CampoTexto.css";
+
+const CampoTexto = (props) => {
+  const placeholderModificada = `${props.placeholder}...`;
+
+  const [valor, setValor] = useState("");
+
+  const aoDigitado = (evento) => {
+    props.aoAlterado(evento.target.value);
+    setValor(evento.target.value);
+  };
+
+  return (
+    <div className="campo-texto">
+      <label>{props.label}</label>
+      <input
+        value={props.valor}
+        onChange={aoDigitado}
+        required={props.obrigatorio}
+        placeholder={placeholderModificada}
+      />
+    </div>
+  );
+};
+
+export default CampoTexto;
